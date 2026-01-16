5480f5ac426b60d6938d8a94b73f201141ecc125
VillalbaMonjeIkerSalvatore <20253rd010@utez.edu.mx>
2026-01-16 13:29:41 -0600

    Add files via upload

--- productos.java
+++ productos.java
@@ -1,10 +1,10 @@
 import java.util.Scanner; //importe la biblioteca
 public class productos{
 public static void main(String[]args){ //funcion principal
-Scanner leer= new Scanner(System.in);
+Scanner leer= new Scanner(System.in); 
 
 double producto1, producto2, producto3, producto4, producto5;
-double descuento=0.10;
+double DESCUENTO=0.10;
 double resultado;
 
 //definimos las variables que ocuparemos y una constante la cual es descuento
@@ -24,7 +24,7 @@ producto4=leer.nextDouble();
 System.out.println("cual es el precio de tu quinto producto");
 producto5=leer.nextDouble();
 
-resultado=(producto1+producto2+producto3+producto4+producto5)*descuento;
+resultado=(producto1+producto2+producto3+producto4+producto5)*DESCUENTO; //muestro los resultados 
 System.out.println("el descuento aplicado a los productos es :"+ resultado);
 
 leer.close();
